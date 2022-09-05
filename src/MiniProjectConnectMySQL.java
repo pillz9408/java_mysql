7a98fcdad7e2a144352a6de310080701f66ee9e7
pillz9408 <pillz9408@gmail.com>
2022-09-05 09:39:17 +0900

    commit

---
+++ src/MiniProjectConnectMySQL.java
@@ -0,0 +1,17 @@
+public class MiniProjectConnectMySQL {
+    public static void main(String[] args) {
+        // run MySQL Workbench
+        // connect to DataBase
+        // Hostname: localhost  port: 3306
+        // Username: root
+        // Default Schema : world
+        // password : tbrs00002b
+        // access to 'World' DB
+        // Select * from City
+        // print World.City
+
+
+        return;
+    }
+    
+}
